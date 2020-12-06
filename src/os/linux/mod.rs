10ed5c4494a77bca760af9dab7d8430aef8b7ec3
Gustavo Moitinho Trindade <gustavo.moitinho@outlook.com>
2020-12-06 07:47:05 -0300

    Open the editor and android studio based on the FreeDesktop entries
    This can make good use of more testing. But now it can probably do all the apple
    version did. Even if it's not as clean. There is a lot of parsing of the FreeDesktop
    files, and the code for finding the application by name can be very slow, although
    it didn't take long at all on the tests I did.

--- src/os/linux/mod.rs
+++ src/os/linux/mod.rs
@@ -1,52 +1,88 @@
+mod xdg;
+
 use std::{
     ffi::OsStr,
     fmt::{self, Display},
-    path::{Path, PathBuf},
+    path::Path,
 };
-
-pub type OsErrorCode = isize;
 
 #[derive(Debug)]
 pub enum DetectEditorError {
-    LookupFailed(OsErrorCode),
+    LookupFailed,
 }
 
 impl Display for DetectEditorError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Self::LookupFailed(err) => write!(f, "{}", err),
+            Self::LookupFailed => write!(f, "Lookup failed"),
         }
     }
 }
 
 #[derive(Debug)]
 pub enum OpenFileError {
-    PathToUrlFailed { path: PathBuf },
-    LaunchFailed(OsErrorCode),
+    LaunchFailed,
 }
 
 impl Display for OpenFileError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Self::PathToUrlFailed { path } => {
-                write!(f, "Failed to convert path {:?} into a `CFURL`.", path)
-            }
-            Self::LaunchFailed(status) => write!(f, "Status code {}", status),
+            Self::LaunchFailed => write!(f, "Launch failed"),
         }
     }
 }
 
 #[derive(Debug)]
 pub struct Application {
+    exec_command: String,
 }
 
 impl Application {
     pub fn detect_editor() -> Result<Self, DetectEditorError> {
-        unimplemented!()
+        let entry = {
+            // Try a rust code editor, then a plain text editor, if neither are available,
+            // then return an error.
+            if let Some(rust_entry) = xdg::query_mime_entry("text/rust") {
+                rust_entry
+            } else if let Some(plain_entry) = xdg::query_mime_entry("text/plain") {
+                plain_entry
+            } else {
+                return Err(DetectEditorError::LookupFailed);
+            }
+        };
+
+        for dir in xdg::get_xdg_data_dirs() {
+            // Look at the applications folder of that dir
+            let dir = dir.join("applications");
+            if let Some(result) = xdg::find_entry_in_dir(&dir, &entry) {
+                return Ok(Self {
+                    exec_command: xdg::command_from_freedesktop_entry(&result)
+                        .ok_or(DetectEditorError::LookupFailed)?
+                })
+            }
+        }
+
+        Err(DetectEditorError::LookupFailed)
     }
 
-    pub fn open_file(&self, _path: impl AsRef<Path>) -> Result<(), OpenFileError> {
-        unimplemented!()
+    pub fn open_file(&self, path: impl AsRef<Path>) -> Result<(), OpenFileError> {
+        let path = path.as_ref();
+        let path_str = path.to_string_lossy(); // Ugh, lossy
+
+        let command = xdg::build_command(&self.exec_command, &path_str)
+            .ok_or(OpenFileError::LaunchFailed)?;
+
+        // I'm having a problem creating a dettached process with bossy,
+        // but this should work and be well supported on linux systems,
+        // as "sh" and no "nohup" are pretty standard.
+        bossy::Command::impure("sh")
+            .add_args(&["-c", &format!("nohup {} > /dev/null &", command)])
+            .set_stdout(bossy::Stdio::null())
+            .set_stderr(bossy::Stdio::null())
+            .run_and_wait()
+            .map_err(|_| OpenFileError::LaunchFailed)?;
+
+        Ok(())
     }
 }
 
@@ -54,11 +90,29 @@ pub fn open_file_with(
     application: impl AsRef<OsStr>,
     path: impl AsRef<OsStr>,
 ) -> bossy::Result<()> {
-    // I have to test whether this is really it
-    bossy::Command::impure("nohup")
-        .with_args(&[application.as_ref(), path.as_ref()])
-        .set_stdout(bossy::Stdio::null())
-        .set_stderr(bossy::Stdio::null())
-        .run_and_wait()?;
+    // I really dislike this lossy conversions
+    // I feel "less" bad about it after I learned firefox also does it
+    // https://support.mozilla.org/en-US/kb/utf-8-only-file-paths
+    // But it still isn't perfect.
+    let app_str = application.as_ref().to_string_lossy();
+    let path_str = path.as_ref().to_string_lossy();
+
+    for dir in xdg::get_xdg_data_dirs() {
+        let dir = dir.join("applications");
+        if let Some(entry) = xdg::find_entry_by_app_name(&dir, &app_str) {
+            let exec_str = entry.section("Desktop Entry")
+                .attr("Exec")
+                // If there is no attribute Exec we may as well try our luck with the app_str.
+                // The main reason is that I don't want to change the function return type.
+                .unwrap_or(&app_str);
+
+            bossy::Command::impure("sh")
+                .add_args(&["-c", &format!("nohup \"{}\" \"{}\" > /dev/null &", exec_str, path_str)])
+                .set_stdout(bossy::Stdio::null())
+                .set_stderr(bossy::Stdio::null())
+                .run_and_wait()?;
+            break;
+        }
+    }
     Ok(())
 }
---
+++ src/os/linux/xdg.rs
@@ -0,0 +1,127 @@
+use std::{
+    path::{Path, PathBuf},
+    env,
+};
+use freedesktop_entry_parser::Entry as FreeDesktopEntry;
+use freedesktop_entry_parser::parse_entry;
+use regex::*;
+
+// Detects which .desktop file contains the data on how to handle a given
+// mime type (like: "with which program do I open a text/rust file?")
+pub fn query_mime_entry(mime_type: &str) -> Option<PathBuf> {
+    let out = bossy::Command::impure("xdg-mime")
+        .add_args(&["query", "default", mime_type])
+        .run_and_wait_for_output()
+        .ok()?;
+    if let Ok(out_str) = out.stdout_str() {
+        log::debug!("query_mime_entry got output \"{}\"", out_str);
+        if !out_str.is_empty() {
+            return Some(out_str.trim().into())
+        }
+    }
+    None
+}
+
+// Returns the first entry on that directory whose filename is equal to target.
+//
+// This spec is what makes me believe the search is recursive:
+// https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html
+// This other one does not give that idea:
+// https://specifications.freedesktop.org/menu-spec/latest/ar01s02.html
+pub fn find_entry_in_dir(dir_path: &Path, target: &Path) -> Option<PathBuf> {
+    for entry in dir_path.read_dir().ok()? {
+        if let Ok(entry) = entry {
+            // If it is a file with that same _filename_ (not full path)
+            if entry.path().is_file() && entry.file_name() == target {
+                return Some(entry.path().into())
+            } else if entry.path().is_dir() {
+                // I think if there are any dirs on that directory we have to
+                // recursively search on them
+                if let Some(result) = find_entry_in_dir(&entry.path(), target) {
+                    return Some(result);
+                }
+            }
+        }
+    }
+    None
+}
+
+// Creates a command from the path to a freedesktop "Desktop Entry" file.
+// These kind of files are generally named "something.desktop"
+pub fn command_from_freedesktop_entry(entry: &Path) -> Option<String> {
+    let parsed = parse_entry(entry).ok()?;
+
+    let exec_command = parsed.section("Desktop Entry")
+        .attr("Exec")?
+        .into();
+
+    Some(exec_command)
+}
+
+pub fn find_entry_by_app_name(dir_path: &Path, app_name: &str) -> Option<FreeDesktopEntry> {
+    for entry in dir_path.read_dir().ok()? {
+        if let Ok(entry) = entry {
+            // If it is a file we open it
+            if entry.path().is_file() {
+                if let Ok(parsed) = parse_entry(entry.path()) {
+                    if let Some(name) = parsed.section("Desktop Entry").attr("Name") {
+                        if name == app_name {
+                            return Some(parsed);
+                        }
+                    }
+                }
+            } else if entry.path().is_dir() {
+                // Recursively keep searching if it is a directory
+                if let Some(result) = find_entry_by_app_name(&entry.path(), app_name) {
+                    return Some(result);
+                }
+            }
+        }
+    }
+    None
+}
+
+// The exec field of the FreeDesktop entry may contain some flags that need to
+// be replaced by parameters or even other stuff. The other things are still
+// not implemented
+pub fn build_command(command: &str, argument: &str) -> Option<String> {
+    let mut command = command.to_string();
+
+    let arg_re = Regex::new(r"%u|%U|%f|%F").unwrap();
+    while let Some(mat) = arg_re.find(&command) {
+        let start = mat.start();
+        let end = mat.end();
+        command.replace_range(start..end, argument);
+    }
+
+    Some(command)
+}
+
+// Returns a vector of all the relevant xdg desktop application entries
+// Check out:
+// https://wiki.archlinux.org/index.php/XDG_Base_Directory
+// That explains the default values and the relevant variables.
+pub fn get_xdg_data_dirs() -> Vec<PathBuf> {
+    let mut result = Vec::new();
+
+    if let Ok(home) = env::var("HOME") {
+        let home: PathBuf = home.into();
+        let xdg_data_home: PathBuf = if let Ok(var) = env::var("XDG_DATA_HOME") {
+            var.into()
+        } else {
+            home.join(".local/share") // The default
+        };
+        result.push(xdg_data_home);
+    }
+
+    if let Ok(var) = env::var("XDG_DATA_DIRS") {
+        let entries = var.split(":").map(|dirname| PathBuf::from(dirname));
+        result.extend(entries);
+    } else {
+        // These are the default ones we'll use in case the var is not set
+        result.push(PathBuf::from("/usr/local/share"));
+        result.push(PathBuf::from("/usr/share"));
+    };
+
+    result
+}
